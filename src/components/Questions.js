5d0a43d66ce24a163f219ef8553b3a4ee31f49ec
Pawel Owiesek <Owiesek.p@gmail.com>
2025-02-20 17:01:20 +0100

    Add Questions component to display question text and options

---
+++ src/components/Questions.js
@@ -0,0 +1,12 @@
+import Options from "./Options";
+
+function Questions({ questions, dispatch, answer }) {
+  return (
+    <div>
+      <h4>{questions.question}</h4>
+      <Options dispatch={dispatch} answer={answer} questions={questions} />
+    </div>
+  );
+}
+
+export default Questions;

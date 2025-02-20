8b4b7ee9f86276adcd43c42ed4b47750746bea55
Pawel Owiesek <Owiesek.p@gmail.com>
2025-02-20 17:00:10 +0100

    Add App and StartScreen components for quiz functionality

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,14 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Questions from "./Questions";
+import NextButton from "./NextButton";
 
 const initialState = {
   questions: [],
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -31,13 +34,30 @@ function reducer(state, action) {
         status: "active",
       };
     }
+    case "newAnswer": {
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    }
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -61,7 +81,16 @@ export default function App() {
           <StartScreen dispatch={dispatch} numQuestions={numQuestions} />
         )}
         {status === "active" && (
-          <Questions index={index} questions={questions[index]} />
+          <>
+            {" "}
+            <Questions
+              index={index}
+              dispatch={dispatch}
+              answer={answer}
+              questions={questions[index]}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
